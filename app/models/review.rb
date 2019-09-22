627c15bf5865323b7ae795476982744dd0ec43a1
Anderson Carubelli <acarubelli@gmail.com>
2019-09-22 15:49:07 -0300

    fix
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true, numericality: { only_integer: true, grater_than: 0, less_than_or_equal_to: 5 }
   validates :description, presence: true, length: { minimum: 50 }
-  validates :user_id, uniqueness: { scope: [ :reviewable_type, reviewable_id], message: "can add only one review per resource" }
+  validates :user_id, uniqueness: { scope: [:reviewable_type, :reviewable_id], message: "can add only one review per resource" }
 end
--- app/models/serie.rb
+++ app/models/serie.rb
@@ -4,7 +4,7 @@ class Serie < ApplicationRecord
   multisearchable against: [:title]
 
   belongs_to :category
-  has_many :reviews, as :reviewable
+  has_many :reviews, as: :reviewable
   has_many :episodes, -> { order(:episode_number) }, class_name: "Movie", dependent: :destroy
   belongs_to :last_watched_episode, class_name: "Movie", optional: true
 
