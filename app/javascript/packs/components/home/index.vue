4a5653a4eb2bc8bf667dcfbfef1a4b68ffdcda1c
Anderson Carubelli <acarubelli@gmail.com>
2019-09-29 19:40:43 -0300

    added featured component
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- app/javascript/packs/components/home/index.vue
+++ app/javascript/packs/components/home/index.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>
-    <h4 class="white--text">Wellcome to HomePage</h4>
-  </div>
+  <v-layout row wrap justify-center>
+    <v-flex xs12>
+      <Featured :movie="featuredMovie"/>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+  import Featured from './_featured.vue'
+
+  // ------- Dados Fake apenas para testarmos o layout -------- //
+  const featuredMovie = {
+    id: 1,
+    title: 'Criando uma API completa com Ruby On Rails',
+    description: `Saber como criar e consumir API 's é
+      fundamental para qualquer programador, então nessa pequena
+      série nós vamos ver o que é essencial para criar uma usando RoR.`,
+    featured_thumbnail_url: 'https://onebitcode.com/wp-content/uploads/2018/05/bg-example.png'
+  }
+
+
   export default {
     data () {
-      return { }
+      return {
+        // ------- Dados Fake apenas para testarmos o layout -------- //
+        featuredMovie: featuredMovie,
+      }
+    },
+    components: {
+      Featured
     }
   }
 </script>
