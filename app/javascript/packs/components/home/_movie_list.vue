82273e7a47bf29a5eff610ad983df685c5adce2a
Anderson Carubelli <acarubelli@gmail.com>
2019-09-29 21:43:01 -0300

    add movie menu
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- app/javascript/packs/components/home/_movie_list.vue
+++ app/javascript/packs/components/home/_movie_list.vue
@@ -3,20 +3,25 @@
     <v-layout row wrap class="movie_list">
       <v-flex xs12>
         <p class='name'>{{ name }}</p>
-        <slick ref= "slick" :options="slickOptions">
-          <a v-for= "movie in movies"
-            :key="movie.id"
-            href= "#"
-            ><img :src="movie.thumb_url">
-          </a>
-        </slick>
+          <slick ref= "slick" :options="slickOptions">
+            <a v-for="movie in movies"
+              :key="movie.id"
+              href="#"
+              @click= "openDetails($event, movie.id)"
+              >
+              <img :src="movie.thumb_url">
+            </a>
+          </slick>
       </v-flex>
     </v-layout>
+    <MovieMenu v-if="menuOpen" :id="movieId"
+      :type="movieType" :closeDetails="closeDetails"/>
   </div>
 </template>
 
 <script>
-  import Slick from 'vue-slick';
+  import MovieMenu from './_movie_menu.vue';
+  import Slick from 'vue-slick' ;
 
   export default {
     props: {
@@ -31,9 +36,12 @@
     },
     data () {
       return {
-      slickOptions: {
+        menuOpen: false,
+        movieId: null,
+        movieType: 'serie',
+        slickOptions: {
           slidesToShow: 4,
-          arrows: false,
+          arrows: false ,
           responsive: [
             {
               breakpoint: 1024,
@@ -54,21 +62,36 @@
       }
     },
     components: {
-      Slick
+      Slick,
+      MovieMenu
+    },
+    methods: {
+      openDetails(e, id) {
+        e.preventDefault();
+        if (this.menuOpen == true && this.movieId == id){
+          this.closeDetails();
+        }else{
+          this.movieId = parseInt(id);
+          this.menuOpen = true;
+        }
+      },
+      closeDetails(){
+        this.menuOpen = false ;
+      }
     }
   }
 </script>
 
 <style scoped>
-  .movie_list{
-    margin-left: 4%;
-    margin-top: 30px;
-    margin-bottom: 20px;
-  }
-  .name {
-    font-size: 24px !important;
-    font-family: 'Source Sans Pro';
-    font-weight: 600;
-    color: white;
-  }
+ .movie_list{
+   margin-left: 4%;
+   margin-top: 30px;
+   margin-bottom: 20px;
+ }
+ .name {
+   font-size: 24px !important;
+   font-family: 'Source Sans Pro';
+   font-weight: 600;
+   color: white;
+ }
 </style>
